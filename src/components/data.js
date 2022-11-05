41e376ed211bd77777c809192d064d312b8cedfb
AlbertoLanchas <albertolsn7@gmail.com>
2022-11-05 13:00:46 +0100

    radio buttons styles inital fixed
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,7 @@
 export const circles = [
-  { id: "0", colour: "red" },
-  { id: "1", colour: "red" },
-  { id: "2", colour: "red" },
-  { id: "3", colour: "red" },
-  { id: "4", colour: "red" },
+  { id: "0", colour: "" },
+  { id: "1", colour: "" },
+  { id: "2", colour: "" },
+  { id: "3", colour: "" },
+  { id: "4", colour: "" },
 ];
